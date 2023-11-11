813ed3ae2a305ef8504753c1739b733391b1ee0f
AdelaidaGomez <agomezvieco@gmail.com>
2023-11-11 12:01:06 -0500

    Rutas de navegacion a cada html desde app.js

--- app.js
+++ app.js
@@ -18,3 +18,19 @@ app.listen(3050, () => {
 app.get("/", (req, res) => {
     res.sendFile(path.resolve(__dirname, "views/index.html"))
 })
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/login.html"))
+})
+
+app.get("/productCart", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/productCart.html"))
+})
+
+app.get("/productDetail", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/productDetail.html"))
+})
+
+app.get("/register", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "views/register.html"))
+})
