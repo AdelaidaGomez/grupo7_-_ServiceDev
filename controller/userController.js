3966e0c4582668beaee6c85e097e0df93ea0be12
Diana Paola Orjuela <148511238+PaolaO83@users.noreply.github.com>
2023-12-26 22:57:10 -0500

    Update userController.js
    adicion de callback para registro de usuarios

--- controller/userController.js
+++ controller/userController.js
@@ -13,6 +13,25 @@ let userController = {
         //res.sendFile(path.resolve(__dirname, '../src/views/register.ejs'))
         res.render('register')
     },
+       createRegister: function(req, res) {
+        //res.send(req.body)
+        // Traigo constante de servicios y transformo al JSON en un array
+         const services = JSON.parse(fs.readFileSync(servicesFilePath, 'utf-8'));
+         // Incluyo la info del formulario y creo el objeto literal a sumar al array
+         const newService = {
+             id: services[services.length - 1].id + 1,   // Tomo el ultimo servicio, agarro su id y le sumo 1.
+             name: req.body.name,
+             email: req.body-email,
+             password: req.body.password,
+             confirm_pass: req.body.confirm_pass,
+             type_user: req.body.type_user,
+           
+         }
+         services.push(newService); // Pusheo el objeto literal al array
+         fs.writeFileSync(servicesFilePath, JSON.stringify(services, null, " ")); // Transformo a JSON y sobreescribo el JSON
+         res.redirect("/"); // Mostramos al usuario la vista principal
+         
+    }
  }
 
 // Exportamos 
