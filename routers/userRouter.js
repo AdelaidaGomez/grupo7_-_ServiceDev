147bbce4288986ebf2338c1a2ac8a01bd1a5bab9
Diana Paola Orjuela <148511238+PaolaO83@users.noreply.github.com>
2023-12-26 23:00:35 -0500

    Update userRouter.js
    se agrega metodo post en el ruteador del registro de usuarios

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -9,7 +9,7 @@ const userController = require('../controller/userController.js');
 
 router.get('/login', userController.login); // todos los servicios, recordar que como es otro archivo se inicia con / ya ue definimos en app que tiene /services 
 router.get('/register', userController.register); //Recordar que para entrar a este la ruta debe ser: Servido/services/productCart
-
+router.post('/register', userController.createRegister); //se establece el metodo post para enviar los datos registrados en el formulario
 // Ruteo de formulario create
 
 // Exportamos Router
