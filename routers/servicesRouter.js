ec461df423b749ab91c0a4e07ef9c6df3629ed9d
martinhaimovich2010 <mhaimov1995@gmail.com>
2024-02-25 13:29:11 -0300

    bases del CRUD de services con la database, pero tira error cuando entro a la pagina y selecciono la pestaña de services

--- routers/servicesRouter.js
+++ routers/servicesRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 // Requerimos el router desde express
 const router = express.Router()
 
-// Requerimos el objeto literal para products desde controllers
+// Requerimos el objeto literal para products desde CONTROLLERS
 const servicesController = require('../controller/servicesController.js');
 // middleWare no esta logheado no habilitar productCart
 let authMiddleware = require("../middlewares/authMiddleware");
@@ -27,21 +27,21 @@ const storage = multer.diskStorage({
 // Asignamos en una constante a la ejecucion de multer
 const uploadFile = multer({storage: storage});
 
-
-
-router.get('/', servicesController.allProducts); // todos los servicios, recordar que como es otro archivo se inicia con / ya ue definimos en app que tiene /services 
+// LEER SERVICIOS
+router.get('/', servicesController.allProducts); // todos los servicios, recordar que como es otro archivo se inicia con / ya que definimos en app que tiene /services 
 router.get('/productCart', authMiddleware, servicesController.productCart); //Recordar que para entrar a este la ruta debe ser: Servidor/services/productCart
 
 router.get('/serviceDetail/:id', servicesController.detail)
 
-// Ruteo de formulario create
+// CREAR SERVICIO
 router.get("/create", servicesController.create) // Para devolverle al usuario el formulario para crear servicio
 router.post("/create", uploadFile.single("image"), servicesController.processCreate) // Para agregar el servicio creado
 
-// Ruteo de formulario edit
+// EDITAR SERVICIO
 router.get("/edit/:id", servicesController.edit) // Para devolverle al usuario el formulario para editar servicio
 router.put("/edit/:id", uploadFile.single("image"), servicesController.processEdit) // Para actualizar el producto editado
 
+// ELIMINAR SERVICIO
 router.delete("/delete/:id", servicesController.destroy) //Borrar un servicio
 
 // Exportamos Router
